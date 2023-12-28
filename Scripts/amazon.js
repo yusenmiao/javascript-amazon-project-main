5617cfcc218ee3ed135f4347de939c41d220203d
Leo <mirror0325@gmail.com>
2023-12-28 13:50:20 -0800

    lesson 14. make some functions and move a function to cart.js

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,3 +1,6 @@
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -27,7 +30,7 @@ products.forEach((product) => {
     <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
 
     <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -43,7 +46,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png" />
         Added
     </div>
@@ -60,43 +63,71 @@ products.forEach((product) => {
 // use DOM to find the target page area
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+/** Function: Update the number of the cart icon in the
+       website (top right corner)  */
+// sum up all the item's quantity
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  // update the number of the cart icon in the website (top right corner)
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+/**
+ * Function: add "Added" pop-up message when users click "add to Cart" button
+ */
+// We're going to use an object to save the timeout ids.
+// The reason we use an object is because each product
+// will have its own timeoutId. So an object lets us
+// save multiple timeout ids for different products.
+// For example:
+// {
+//   'product-id1': 2,
+//   'product-id2': 5,
+//   ...
+// }
+// (2 and 5 are ids that are returned when we call setTimeout).
+const addedMessageTimeouts = {};
+function showAddedPopUpMessage(productId) {
+  // find the added to cart class
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  // add a new class by using .calssList.add()
+  addedMessage.classList.add("added-to-cart-visible");
+
+  /** after 2 seconds the "Added" message will disappear */
+  // Check if there's a previous timeout for this
+  // product. If there is, we should stop it.
+  const previousTimeoutId = addedMessageTimeouts[productId];
+  if (previousTimeoutId) {
+    clearTimeout(previousTimeoutId);
+  }
+
+  const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove("added-to-cart-visible");
+  }, 2000);
+
+  // Save the timeoutId for this product
+  // so we can stop it later if we need to.
+  addedMessageTimeouts[productId] = timeoutId;
+}
+
 /**
  when users click "Add to Cart" button, 
- we update the cart list with proudctId and update the quantity
+ we update the cart list with proudctId and update the quantity.
+ The button.dataset.productId (dataset is default) is the way we 
+ can find the productId check 
+ "<button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id="${product.id}">"
  */
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
-    let matchingItem;
-
-    /**
-     Loop through the items in cart to find if the current new productId,
-     if it's in the cart, we find the matchingItem, then we only 
-     need to update the quantity of that item
-     **/
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    // update the number of the cart icon in the website
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
+    showAddedPopUpMessage(productId);
   });
 });
--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,30 @@
-const cart = [];
+export const cart = [];
+
+/**
+ Function: Loop through the items in cart to find if the current new productId,
+if it's in the cart, we find the matchingItem, then we only 
+need to update the quantity of that item
+**/
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantity,
+    });
+  }
+}
